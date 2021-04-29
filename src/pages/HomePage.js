9780276afe8a57dfd271f5b55428dda7b0b86bd6
apbank2238 <bayulgang@hotmail.com>
2021-04-30 00:29:54 +0700

    create HomePage.js and edit App.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className="body">
+
+      <div className="header">
+
+        <div classNam="header-left"></div>
+        <div classNam="header-center"></div>
+        <div classNam="header-right"></div>
+
+      </div>
+
+      <div className="content">
+
+        <div classNam="content-left"></div>
+        <div classNam="content-center"></div>
+        <div classNam="content-right"></div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default HomePage
