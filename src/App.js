e7687f8b047a0350abc33b2a70702b480860f121
SupawiriyaSaranarak <xaviel2@gmail.com>
2021-05-10 15:40:15 +0700

    create layout authcontext

--- src/App.js
+++ src/App.js
@@ -2,16 +2,50 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
+import { useContext } from "react";
+import { AuthContext } from "./contexts/AuthContextProvider";
+
 import HomePage from "./pages/HomePage";
 
+const privateRoutes = [
+  // {
+  //   path: "/",
+  //   component: Home,
+  // },
+  // {
+  //   path: "/profile",
+  //   component: Profile,
+  // },
+];
+
+const publicRoutes = [
+  // {
+  //   path: "/register",
+  //   component: Auth,
+  // },
+  {
+    path: "/",
+    component: HomePage,
+  },
+];
+
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={"/"} component={HomePage} />
-        <Redirect to="/" />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      {isAuthenticated &&
+        privateRoutes.map((el, index) => (
+          <Route key={index} exact path={el.path} component={el.component} />
+        ))}
+
+      {!isAuthenticated &&
+        publicRoutes.map((el, index) => (
+          <Route key={index} exact path={el.path} component={el.component} />
+        ))}
+      <Route exact path="/" component={HomePage} />
+
+      <Redirect to="/" />
+    </Switch>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,45 +18,16 @@ function HomePage() {
     //   </div>
     // </div>
 
-    <nav class="flex items-center justify-between flex-wrap bg-teal p-6 bg-red-600">
-      <div class="flex items-center flex-no-shrink text-black mr-6">
-        <img src={LOGO} alt="logo" />
+    <div>
+      <div className="header">Header</div>
+      <div className="content-body">
+        <div className="margin-right"></div>
+        <div className="content-body-room">Room</div>
+        <div className="content-body-topic">Topic</div>
+        <div className="content-body-pin">Pin</div>
+        <div className="margin-left"></div>
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
-          >
-            Docs
-          </a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4"
-          >
-            Examples
-          </a>
-          <a
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white"
-          >
-            Blog
-          </a>
-        </div>
-        <div>
-          <a
-            href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 lg:mt-0"
-          >
-            Download
-          </a>
-        </div>
-      </div>
-      <div class="flex items-center flex-no-shrink text-black mr-6">
-        <img src={LOGO} alt="logo" />
-        <span>Username</span>
-      </div>
-    </nav>
+    </div>
   );
 }
 
