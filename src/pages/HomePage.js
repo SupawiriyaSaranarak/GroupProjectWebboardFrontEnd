9f32887ac09102ea7f84f4fdbf159e8165d0aefd
SupawiriyaSaranarak <xaviel2@gmail.com>
2021-05-12 17:56:12 +0700

    add topiclist component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,14 @@
 import React from "react";
-
-import LOGO from "../img/LOGO.png";
+import TopicList from "../component/TopicList";
 
 function HomePage() {
   return (
-    // <div className="body">
-    //   <div className="header">
-    //     <div className="header-left"></div>
-    //     <div className="header-center"></div>
-    //     <div className="header-right"></div>
-    //   </div>
-
-    //   <div className="content">
-    //     <div className="content-left"></div>
-    //     <div className="content-center"></div>
-    //     <div className="content-right"></div>
-    //   </div>
-    // </div>
-
     <div>
       <div className="header">Header</div>
       <div className="content-body">
         <div className="margin-right"></div>
         <div className="content-body-room">Room</div>
-        <div className="content-body-topic">Topic</div>
+        <TopicList className="content-body-topic" />
         <div className="content-body-pin">Pin</div>
         <div className="margin-left"></div>
       </div>
