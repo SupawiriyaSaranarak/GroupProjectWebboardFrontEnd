3b16f26964d51b30f268cd4178aa9022edb61e46
apbank2238 <bayulgang@hotmail.com>
2021-05-15 01:51:44 +0700

    npm i react-modal

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,20 @@
 import React from "react";
-import Navbar from "../component/navbar"
+import Navbar from "../component/navbar";
 import TopicList from "../component/TopicList";
 import PinBar from "../components/PinBar";
 import RoomBar from "../components/RoomBar";
 
 import LOGO from "../img/LOGO.png";
-import { HomeIcon,PencilIcon } from "@heroicons/react/outline";
+import { HomeIcon, PencilIcon } from "@heroicons/react/outline";
+import { Modal } from "@material-ui/core";
 
 function HomePage() {
   return (
     <div>
-      <div style={{ height: "100px",padding:"30px" }} className="header">
-        <Navbar Icon={HomeIcon} Icon2={PencilIcon }/>
+      <div style={{ height: "100px", padding: "30px" }} className="header">
+        <Navbar Icon={HomeIcon} Icon2={PencilIcon} />
       </div>
-      {/* <div className="content-body">
+      <div className="content-body">
         <div className="margin-right"></div>
         <div className="content-body-room">
           <RoomBar />
@@ -23,7 +24,8 @@ function HomePage() {
           <PinBar />
         </div>
         <div className="margin-left"></div>
-      </div> */}
+      </div>
+      <Modal></Modal>
     </div>
   );
 }
