0283e9f2571c5b42a4ca16edc5440ec8c33b40e8
apbank2238 <bayulgang@hotmail.com>
2021-05-21 03:47:13 +0700

    finish pinBar

--- src/components/PinItems.js
+++ src/components/PinItems.js
@@ -1,17 +1,22 @@
 import React from "react";
 
+import pinIcon from "../img/office-pin.png";
+import pinedIcon from "../img/office-pin-red.png";
+
 function PinItems(props) {
+  const showTopicText = props.item.Topic.topicName.substr(0, 13);
+
   return (
     <div className="roomBar-container-contentBox">
       <div className="pinBar-container-contentBox-inside-iconImg-Bg">
         <img
-          src={props.pinIcon}
-          alt={"roomIcon"}
+          src={pinedIcon}
+          alt={"pinIcon"}
           className="roomBar-container-contentBox-inside-iconImg"
         />
       </div>
       <div className="roomBar-container-contentBox-inside-textBox">
-        PIN CONTENT
+        {showTopicText + "..."}
       </div>
     </div>
   );
