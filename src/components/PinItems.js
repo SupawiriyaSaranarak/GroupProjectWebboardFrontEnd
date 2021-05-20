c2d565032858c9b2508ed69a624c99f91548d067
apbank2238 <bayulgang@hotmail.com>
2021-05-21 03:52:02 +0700

    bank commit pinBar

--- src/components/PinItems.js
+++ src/components/PinItems.js
@@ -6,6 +6,14 @@ import pinedIcon from "../img/office-pin-red.png";
 function PinItems(props) {
   const showTopicText = props.item.Topic.topicName.substr(0, 13);
 
+  const handlerDeletePin = (e, pinned) => {
+    console.log("handlerDeletePin", pinned);
+  };
+
+  const handlerGoToPinTopic = (e, topic) => {
+    console.log("handlerGoToPinTopic", topic);
+  };
+
   return (
     <div className="roomBar-container-contentBox">
       <div className="pinBar-container-contentBox-inside-iconImg-Bg">
@@ -13,9 +21,13 @@ function PinItems(props) {
           src={pinedIcon}
           alt={"pinIcon"}
           className="roomBar-container-contentBox-inside-iconImg"
+          onClick={(e) => handlerDeletePin(e, props.item)}
         />
       </div>
-      <div className="roomBar-container-contentBox-inside-textBox">
+      <div
+        className="roomBar-container-contentBox-inside-textBox"
+        onClick={(e) => handlerGoToPinTopic(e, props.item.Topic)}
+      >
         {showTopicText + "..."}
       </div>
     </div>
